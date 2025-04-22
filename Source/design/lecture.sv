e7efa84bb4baa99153659d3eccd357cf4d6e4a54
Sebas Meza G <ps4.sebas@yahoo.com>
2025-04-21 23:44:37 -0600

    Sipi

--- Source/design/lecture.sv
+++ Source/design/lecture.sv
@@ -78,39 +78,38 @@ module lecture (
  
     assign columna_presionada_total = columna_presionada0 | columna_presionada1 | columna_presionada2 | columna_presionada3;
 
-    always @(filas_db) begin
-        case({columna_presionada_total, filas_db})
-            8'b1000_1000 : key_pressed = 4'b0001; // columna 0, fila 0 = 1
-            8'b0100_1000 : key_pressed = 4'b0010; // columna 1, fila 0 = 2
-            8'b0010_1000 : key_pressed = 4'b0011; // columna 2, fila 0 = 3
-            8'b0001_1000 : key_pressed = 4'b1010; // columna 3, fila 0 = 10 A
+always_comb begin
+    case({columna_presionada_total, filas_db})
+        8'b1000_1000 : key_pressed = 4'b0001; // columna 0, fila 0 = 1
+        8'b0100_1000 : key_pressed = 4'b0010; // columna 1, fila 0 = 2
+        8'b0010_1000 : key_pressed = 4'b0011; // columna 2, fila 0 = 3
+        8'b0001_1000 : key_pressed = 4'b1010; // columna 3, fila 0 = 10 A
 
-            8'b1000_0100 : key_pressed = 4'b0100; // columna 0, fila 1 = 4
-            8'b0100_0100 : key_pressed = 4'b0101; // columna 1, fila 1 = 5
-            8'b0010_0100 : key_pressed = 4'b0110; // columna 2, fila 1 = 6
-            8'b0001_0100 : key_pressed = 4'b1011; // columna 3, fila 1 = 11 B
+        8'b1000_0100 : key_pressed = 4'b0100; // columna 0, fila 1 = 4
+        8'b0100_0100 : key_pressed = 4'b0101; // columna 1, fila 1 = 5
+        8'b0010_0100 : key_pressed = 4'b0110; // columna 2, fila 1 = 6
+        8'b0001_0100 : key_pressed = 4'b1011; // columna 3, fila 1 = 11 B
 
-            8'b1000_0010 : key_pressed = 4'b0111; // columna 0, fila 2 = 7
-            8'b0100_0010 : key_pressed = 4'b1000; // columna 1, fila 2 = 8
-            8'b0010_0010 : key_pressed = 4'b1001; // columna 2, fila 2 = 9
-            8'b0001_0010 : key_pressed = 4'b1100; // columna 3, fila 2 = 12 C
+        8'b1000_0010 : key_pressed = 4'b0111; // columna 0, fila 2 = 7
+        8'b0100_0010 : key_pressed = 4'b1000; // columna 1, fila 2 = 8
+        8'b0010_0010 : key_pressed = 4'b1001; // columna 2, fila 2 = 9
+        8'b0001_0010 : key_pressed = 4'b1100; // columna 3, fila 2 = 12 C
 
-            8'b1000_0001 : key_pressed = 4'b1101; // columna 0, fila 3 = 13 * D
-            8'b0100_0001 : key_pressed = 4'b0000; // columna 1, fila 3 = 0
-            8'b0010_0001 : key_pressed = 4'b1110; // columna 2, fila 3 = # 14 E
-            //8'b0001_0001 : key_pressed = 4'b1111; // columna 3, fila 3 = 15 F (Tecla D)
-            default: key_pressed = 4'b1111; // Si no hay coincidencia, salida por defecto
+        8'b1000_0001 : key_pressed = 4'b1101; // columna 0, fila 3 = 13 * D
+        8'b0100_0001 : key_pressed = 4'b0000; // columna 1, fila 3 = 0
+        8'b0010_0001 : key_pressed = 4'b1110; // columna 2, fila 3 = # 14 E
+        default: key_pressed = 4'b1111; // Si no hay coincidencia, salida por defecto
+    endcase
+end
 
-        endcase
+
+always_ff @(posedge clk or negedge n_reset) begin
+    if (~n_reset) begin
+        sample <= 4'b1111; // Reset the sample value
+    end else if (key_pressed !== 4'b1111) begin
+        sample <= key_pressed; // Assign key_pressed to sample on clock edge
     end
-
-
-    always @(key_pressed) begin
-        if (key_pressed !== 4'b1111) begin
-            sample <= key_pressed; // Asignar el valor de key_pressed a sample
-        end
-    end
-
+end
 endmodule
  
 
